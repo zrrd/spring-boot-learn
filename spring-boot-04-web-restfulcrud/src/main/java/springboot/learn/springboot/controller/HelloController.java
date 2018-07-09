f7ec43a66d3d967c557bb09af25f745ee44a10e2
邵益炯 <shaoyijiong@worken.cn>
2018-07-09 14:11:17 +0800

    restfulcrud开始

---
+++ spring-boot-04-web-restfulcrud/src/main/java/springboot/learn/springboot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package springboot.learn.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Hello Controller
+ *
+ * @author shaoyijiong
+ * @date 2018/7/6
+ */
+@Controller
+public class HelloController {
+
+  @ResponseBody
+  @RequestMapping("/hello")
+  public String hello(){
+    return "hello";
+  }
+}
