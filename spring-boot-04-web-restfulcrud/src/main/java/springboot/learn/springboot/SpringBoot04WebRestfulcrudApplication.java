0f420489e355a6af25836acf069391754406482f
shaoyijiong <shaoyijiong@worken.com>
2018-07-16 20:04:04 +0800

    logging

--- spring-boot-04-web-restfulcrud/src/main/java/springboot/learn/springboot/SpringBoot04WebRestfulcrudApplication.java
+++ spring-boot-04-web-restfulcrud/src/main/java/springboot/learn/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBoot04WebRestfulcrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
+  }
 }
