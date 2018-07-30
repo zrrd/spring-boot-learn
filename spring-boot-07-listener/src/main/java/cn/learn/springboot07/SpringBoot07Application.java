3a026ce89c7d5c4759a0054b5a8c09a95ab61173
邵益炯 <shaoyijiong@worken.cn>
2018-07-30 17:54:50 +0800

    修改项目名字

--- spring-boot-07-listener/src/main/java/cn/learn/springboot07/SpringBoot07Application.java
+++ spring-boot-07-listener/src/main/java/cn/learn/springboot07/SpringBoot07Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBoot07Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot07Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot07Application.class, args);
+  }
 }
