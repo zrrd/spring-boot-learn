5f2dd1ba5d6c8bed8a950acfe20714202fbc5311
邵益炯 <shaoyijiong@worken.cn>
2018-12-24 10:57:22 +0800

    el 更新代码

--- spring-boot-10-elasticsearch/src/main/java/cn/learn/springboot/repository/ItemRepository.java
+++ spring-boot-10-elasticsearch/src/main/java/cn/learn/springboot/repository/ItemRepository.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 /**
- * .
- *
  * @author shaoyijiong
  * @date 2018/12/18
  */
 public interface ItemRepository extends ElasticsearchRepository<Item, Long> {
 
+  /**
+   * 查找价格区间
+   *
+   * @param price1 价格1
+   * @param price2 价格2
+   * @return 区间商品
+   */
   List<Item> findByPriceBetween(double price1, double price2);
 }
