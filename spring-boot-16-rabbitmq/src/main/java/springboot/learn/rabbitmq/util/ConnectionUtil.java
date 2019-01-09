0fe911b488aa71ef95b4ec65b03161702ee0fcf4
邵益炯 <shaoyijiong@worken.cn>
2019-01-09 15:18:00 +0800

    rabbit mq

---
+++ spring-boot-16-rabbitmq/src/main/java/springboot/learn/rabbitmq/util/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package springboot.learn.rabbitmq.util;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ConnectionUtil {
+
+  /**
+   * 建立与RabbitMQ的连接
+   */
+  public static Connection getConnection() throws Exception {
+    //定义连接工厂
+    ConnectionFactory factory = new ConnectionFactory();
+    //设置服务地址
+    factory.setHost("192.168.56.101");
+    //端口
+    factory.setPort(5672);
+    //设置账号信息，用户名、密码、vhost
+    factory.setVirtualHost("/leyou");
+    factory.setUsername("leyou");
+    factory.setPassword("leyou");
+    // 通过工程获取连接
+    Connection connection = factory.newConnection();
+    return connection;
+  }
+
+}
