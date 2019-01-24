608fbbb62fe7d858f92bcd1140cabaa1a5a0fc15
邵益炯 <shaoyijiong@worken.cn>
2019-01-24 15:15:19 +0800

    表单请求

--- spring-boot-tips/src/main/java/cn/learn/springboot/handlerInterceptor/MyInterceptor.java
+++ spring-boot-tips/src/main/java/cn/learn/springboot/handlerInterceptor/MyInterceptor.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
  * @author 邵益炯
  * @date 2018/9/25
  */
+
+//HandlerInterceptorAdapter 拦截器适配器 只要实现需要实现的方法
 @Slf4j
 public class MyInterceptor implements HandlerInterceptor {
 
