848b438c4a02f04f06cd476b67897607464045e4
邵益炯 <fantasy9417@163.com>
2019-03-31 09:50:25 +0800

    feat(rocketmq) : 添加一个Consumer展示负载均衡的例子

--- spring-boot-09-rocketmq/src/main/java/cn/learn/springboot/base/simple/Consumer2.java
+++ spring-boot-09-rocketmq/src/main/java/cn/learn/springboot/base/simple/Consumer2.java
@@ -12,7 +12,8 @@ import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
  * @author shaoyijiong
  * @date 2019/3/20
  */
-public class Consumer {
+@SuppressWarnings("ALL")
+public class Consumer2 {
 
   public static void main(String[] args) throws MQClientException {
 
@@ -30,20 +31,6 @@ public class Consumer {
     // Register callback to execute on arrival of messages fetched from brokers.
     consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
       System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-      //模拟消息消费失败的场景 失败后rocketmq会稍后再试 默认16次后会放到死信队列
-      //默认重试间隔 1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
-      //消费失败业务逻辑代码返回ReconsumerLater NULL 或是 抛出异常都会导致重试
-/*      if (msgs != null) {
-        //重试次数
-        System.out.println(msgs.get(0).getReconsumeTimes());
-        System.out.println(new String(msgs.get(0).getBody()));
-        throw new RuntimeException();
-      }*/
-      try {
-        Thread.sleep(20000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
       return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
     });
 
