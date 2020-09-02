49e16611ff6524e11c28d3473778fc68ba4becdb
邵益炯 <shaoyijiong@worken.cn>
2020-09-02 14:20:49 +0800

    xxl-job 示例

--- spring-boot-23-xxl-job/src/main/java/cn/learn/springboot/XxlJobConfig.java
+++ spring-boot-23-xxl-job/src/main/java/cn/learn/springboot/XxlJobConfig.java
@@ -30,5 +30,33 @@ public class XxlJobConfig {
     xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
     xxlJobSpringExecutor.setAppname(appname);
     return xxlJobSpringExecutor;
+
+    // XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+    // xxlJob 控制台地址
+    // xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+    // xxlJobSpringExecutor.setAppname(appname);
+    // xxlJobSpringExecutor.setAddress(address);
+    // xxlJobSpringExecutor.setIp(ip);
+    // xxlJobSpringExecutor.setPort(port);
+    // xxlJobSpringExecutor.setAccessToken(accessToken);
+    // xxlJobSpringExecutor.setLogPath(logPath);
+    // xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+
+    /**
+     * 针对多网卡、容器内部署等情况，可借助 "spring-cloud-commons" 提供的 "InetUtils" 组件灵活定制注册IP；
+     *
+     *      1、引入依赖：
+     *          <dependency>
+     *             <groupId>org.springframework.cloud</groupId>
+     *             <artifactId>spring-cloud-commons</artifactId>
+     *             <version>${version}</version>
+     *         </dependency>
+     *
+     *      2、配置文件，或者容器启动变量
+     *          spring.cloud.inetutils.preferred-networks: 'xxx.xxx.xxx.'
+     *
+     *      3、获取IP
+     *          String ip_ = inetUtils.findFirstNonLoopbackHostInfo().getIpAddress();
+     */
   }
 }
